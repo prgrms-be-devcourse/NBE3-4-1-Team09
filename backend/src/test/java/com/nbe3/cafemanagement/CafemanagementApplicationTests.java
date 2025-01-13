dcb29042bcf7b4b8cc8e8a738a9c6e75d1636699
NRKim93 <ning9303@naver.com>
2025-01-13 16:27:41 +0900

    init commit

---
+++ backend/src/test/java/com/nbe3/cafemanagement/CafemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbe3.cafemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafemanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
