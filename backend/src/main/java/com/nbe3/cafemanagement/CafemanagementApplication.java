71fee910f92c25c3cb3bb09e9726638c707091c7
son <handsun000@gmail.com>
2025-01-14 16:35:50 +0900

    EnableJpaAuditing

--- backend/src/main/java/com/nbe3/cafemanagement/CafemanagementApplication.java
+++ backend/src/main/java/com/nbe3/cafemanagement/CafemanagementApplication.java
@@ -2,8 +2,11 @@ package com.nbe3.cafemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 //
 @SpringBootApplication
+@EnableJpaAuditing
 public class CafemanagementApplication {
 
     public static void main(String[] args) {
