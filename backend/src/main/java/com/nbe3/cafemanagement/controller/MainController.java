8420bbfc8104069754963f0eb2f2dbc18cf74b1e
NRKim93 <ning9303@naver.com>
2025-01-15 00:15:30 +0900

    develope checkProduct

--- backend/src/main/java/com/nbe3/cafemanagement/controller/MainController.java
+++ backend/src/main/java/com/nbe3/cafemanagement/controller/MainController.java
@@ -1,15 +1,34 @@
 package com.nbe3.cafemanagement.controller;
 
+import com.nbe3.cafemanagement.domain.Product;
+import com.nbe3.cafemanagement.service.MainService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/master")
 public class MainController {
 
+    private final MainService mainService;
+
+    public MainController(MainService mainService) {
+        this.mainService = mainService;
+    }
+
+    // 상품 목록 페이지
     @GetMapping("/checkProduct")
-    public String checkProduct() {
+    public String checkProduct(Model model) {
+
+        List<Product> products = mainService.getAllProducts();
+
+        System.out.println("상품 목록: " + products);
+
+        model.addAttribute("products", products);
+
         return "main";
     }
 }
