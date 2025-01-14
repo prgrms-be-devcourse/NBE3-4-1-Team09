02951c039f49c8b74ad81e345228343f7a9461b6
son <handsun000@gmail.com>
2025-01-14 17:05:25 +0900

    create orderDetail

---
+++ backend/src/main/java/domain/OrderDetail.java
@@ -0,0 +1,39 @@
+package domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class OrderDetail {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Setter(AccessLevel.PRIVATE)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Product product;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private int price;
+}
