fefe24494c4d4ca83a7ff0daac9096557cd72e0b
son <handsun000@gmail.com>
2025-01-15 15:08:35 +0900

    payment save

--- backend/src/main/java/com/nbe3/cafemanagement/controller/HomeController.java
+++ backend/src/main/java/com/nbe3/cafemanagement/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.nbe3.cafemanagement.controller;
 
+import com.nbe3.cafemanagement.domain.form.PaymentForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")  // 루트 경로 요청 처리
-    public String homePage() {
-        return "main";  // templates 디렉토리의 main.html 파일을 반환
+    public String homePage(PaymentForm paymentForm) {
+        return "redirect:/master/checkProduct";  // templates 디렉토리의 main.html 파일을 반환
     }
 }
