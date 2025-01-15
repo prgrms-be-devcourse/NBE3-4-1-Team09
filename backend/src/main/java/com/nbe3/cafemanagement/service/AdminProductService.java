9a9e9c70435464641cb6efbf0c9cb66bb130af84
haeminK <khm65426542@gmail.com>
2025-01-15 11:32:01 +0900

    상품추가 기능 구현

--- backend/src/main/java/com/nbe3/cafemanagement/service/AdminProductService.java
+++ backend/src/main/java/com/nbe3/cafemanagement/service/AdminProductService.java
@@ -1,9 +1,28 @@
 package com.nbe3.cafemanagement.service;
 
+import com.nbe3.cafemanagement.domain.Product;
+import com.nbe3.cafemanagement.dto.ProductDto;
+import com.nbe3.cafemanagement.repository.AdminProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AdminProductService {
+
+    private final AdminProductRepository adminProductRepository;
+
+    public void create(ProductDto productDto) {
+
+        if (adminProductRepository.existsByName(productDto.getName())) {
+            throw new RuntimeException("중복된 이름입니다.");
+        }
+
+        Product product = new Product();
+        product.setName(productDto.getName());
+        product.setDescription(productDto.getDescription());
+        product.setPrice(productDto.getPrice());
+
+        adminProductRepository.save(product);
+    }
 }
