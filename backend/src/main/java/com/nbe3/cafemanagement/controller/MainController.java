35a28cfd1c845caefd36e6588ecbc890b36ec24e
son <handsun000@gmail.com>
2025-01-15 14:18:28 +0900

    payment

--- backend/src/main/java/com/nbe3/cafemanagement/controller/MainController.java
+++ backend/src/main/java/com/nbe3/cafemanagement/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.nbe3.cafemanagement.controller;
 
+import com.nbe3.cafemanagement.domain.form.PaymentForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 
     @GetMapping("/checkProduct")
-    public String checkProduct() {
+    public String checkProduct(PaymentForm paymentForm) {
         return "main";
     }
 }
--- backend/src/main/java/com/nbe3/cafemanagement/controller/OrderController.java
+++ backend/src/main/java/com/nbe3/cafemanagement/controller/OrderController.java
@@ -1,4 +1,37 @@
 package com.nbe3.cafemanagement.controller;
 
+import com.nbe3.cafemanagement.domain.Order;
+import com.nbe3.cafemanagement.domain.OrderDetail;
+import com.nbe3.cafemanagement.domain.form.PaymentForm;
+import com.nbe3.cafemanagement.service.OrderDetailService;
+import com.nbe3.cafemanagement.service.OrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/order")
+@RequiredArgsConstructor
 public class OrderController {
+    private final OrderService orderService;
+    private final OrderDetailService orderDetailService;
+
+    @PostMapping("/payment")
+    public String payment(@Valid PaymentForm paymentForm,
+                          @RequestParam(name = "totalPrice", defaultValue = "0") int totalPrice,
+                          @RequestParam(name = "summary", defaultValue = "") String summary,
+                          BindingResult bindingResult
+                          ) {
+        if (bindingResult.hasErrors()) {
+            return "main";
+        }
+        Order order = orderService.save(paymentForm.getEmail(), paymentForm.getAddress(), paymentForm.getPostcode(), totalPrice);
+        OrderDetail orderDetail = orderDetailService.save();
+
+        return "redirect:/master/checkProduct";
+    }
 }
