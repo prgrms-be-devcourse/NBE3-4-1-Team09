d1d5b99d40a99f213ab04209ee9c558e685542c7
haeminK <khm65426542@gmail.com>
2025-01-15 14:07:50 +0900

    상품수정 기능 구현

--- backend/src/main/java/com/nbe3/cafemanagement/service/AdminProductService.java
+++ backend/src/main/java/com/nbe3/cafemanagement/service/AdminProductService.java
@@ -25,4 +25,28 @@ public class AdminProductService {
 
         adminProductRepository.save(product);
     }
+
+    public void update(ProductDto productDto, int id) {
+        if (!adminProductRepository.existsById(id)) {
+            throw new RuntimeException("존재하지 않는 상품입니다.");
+        }
+
+        Product product = new Product();
+        product.setId(id);
+        product.setName(productDto.getName());
+        product.setDescription(productDto.getDescription());
+        product.setPrice(productDto.getPrice());
+        adminProductRepository.save(product);
+    }
+
+    public ProductDto getProduct(int id) {
+        Product product = adminProductRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("존재하지 않는 상품 id 입니다, id=%d".formatted(id)));
+
+        return ProductDto.builder()
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
 }
