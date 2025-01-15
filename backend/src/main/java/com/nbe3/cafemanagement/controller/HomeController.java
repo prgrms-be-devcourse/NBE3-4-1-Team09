6b480792e28b9a2201fdaa708237621f9e108a3c
NRKim93 <ning9303@naver.com>
2025-01-15 14:13:54 +0900

    edit HomeController.java

--- backend/src/main/java/com/nbe3/cafemanagement/controller/HomeController.java
+++ backend/src/main/java/com/nbe3/cafemanagement/controller/HomeController.java
@@ -8,6 +8,9 @@ public class HomeController {
 
     @GetMapping("/")  // 루트 경로 요청 처리
     public String homePage() {
-        return "main";  // templates 디렉토리의 main.html 파일을 반환
+
+        // 초안 : templates 디렉토리의 main.html 파일을 반환
+        // 2025-01-15 김누리 수정 : 상품 초기 화면 표시
+        return "redirect:/master/checkProduct";
     }
 }
