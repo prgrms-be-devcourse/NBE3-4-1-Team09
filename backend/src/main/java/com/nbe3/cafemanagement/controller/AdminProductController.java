ad58f3ecb5f82b3f768dca1c20ce60716ea4e5dd
haeminK <khm65426542@gmail.com>
2025-01-15 11:32:01 +0900

    상품추가 기능 구현

--- backend/src/main/java/com/nbe3/cafemanagement/controller/AdminProductController.java
+++ backend/src/main/java/com/nbe3/cafemanagement/controller/AdminProductController.java
@@ -1,8 +1,15 @@
 package com.nbe3.cafemanagement.controller;
 
+import com.nbe3.cafemanagement.dto.ProductDto;
+import com.nbe3.cafemanagement.service.AdminProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,10 +17,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin/product")
 public class AdminProductController {
 
-    @GetMapping("/new")
-    public String create() {
-        return "admin/admin_product";
+    private final AdminProductService adminProductService;
+
+    @GetMapping("/main")
+    public String productMain() {
+        return "admin/product/main";
     }
 
+    @GetMapping("/new")
+    public String createPage(Model model) {
+        model.addAttribute("product", new ProductDto());
+        return "admin/product/new";
+    }
 
+    @PostMapping("/new")
+    public String createProduct(@Valid @ModelAttribute("product") ProductDto product, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "admin/product/new";
+        }
+        adminProductService.create(product);
+        return "redirect:/admin/product/main";
+    }
+
+//    @GetMapping("/edit")
+//    public String editPage() {
+//        return "admin/product/admin_product_edit";
+//    }
 }
--- backend/src/main/java/com/nbe3/cafemanagement/repository/AdminProductRepository.java
+++ backend/src/main/java/com/nbe3/cafemanagement/repository/AdminProductRepository.java
@@ -3,6 +3,9 @@ package com.nbe3.cafemanagement.repository;
 import com.nbe3.cafemanagement.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdminProductRepository extends JpaRepository<Product, Integer> {
+import java.util.Optional;
 
+public interface AdminProductRepository extends JpaRepository<Product, Integer> {
+    boolean existsByName(String name);
+    public Optional<Product> findByName(String name);
 }
