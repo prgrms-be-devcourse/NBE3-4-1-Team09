a4262f008cc570861beb0893576c12ecddb8967a
son <handsun000@gmail.com>
2025-01-15 23:54:45 +0900

    /order/payment orderDetail save 추가

--- backend/src/main/java/com/nbe3/cafemanagement/service/OrderDetailService.java
+++ backend/src/main/java/com/nbe3/cafemanagement/service/OrderDetailService.java
@@ -1,18 +1,43 @@
 package com.nbe3.cafemanagement.service;
 
+import com.nbe3.cafemanagement.domain.Order;
 import com.nbe3.cafemanagement.domain.OrderDetail;
+import com.nbe3.cafemanagement.domain.Product;
+import com.nbe3.cafemanagement.repository.MainRepository;
 import com.nbe3.cafemanagement.repository.OrderDetailRepository;
 import lombok.RequiredArgsConstructor;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class OrderDetailService {
 
     private final OrderDetailRepository orderDetailRepository;
+    private final MainRepository mainRepository;
 
-    public OrderDetail save() {
+    public void save(Order order, String products) {
+        JSONArray jsonArray = new JSONArray(products);
 
-        return null;
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+            Product product = mainRepository.findById(jsonObject.getLong("id"))
+                    .orElseThrow();
+            int quantity = Integer.parseInt(jsonObject.get("quantity").toString());
+            int price = Integer.parseInt(jsonObject.get("price").toString());
+
+            OrderDetail orderDetail = OrderDetail.builder()
+                    .order(order)
+                    .product(product)
+                    .quantity(quantity)
+                    .price(quantity * price)
+                    .build();
+
+            orderDetailRepository.save(orderDetail);
+        }
     }
 }
--- backend/src/main/java/com/nbe3/cafemanagement/service/OrderService.java
+++ backend/src/main/java/com/nbe3/cafemanagement/service/OrderService.java
@@ -7,6 +7,8 @@ import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,15 +16,23 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    public Order save(String email, String address, String postcode, int totalPrice) {
+    public Order save(String email, String address, String postcode, int totalAmount) {
         Order order = Order.builder()
                 .email(email)
                 .address(address + "|" + postcode)
-                .totalPrice(totalPrice)
+                .totalPrice(totalAmount)
                 .orderDate(LocalDate.now())
                 .status(Order.OrderStatus.READY)
                 .build();
         
         return orderRepository.save(order);
+    }
+
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    public Optional<Order> findById(long id) {
+        return orderRepository.findById(id);
     }
 }
