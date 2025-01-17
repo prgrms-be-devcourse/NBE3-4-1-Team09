3c781ae092c549692dbbf29a2111da9b520cfbba
oungsi2000 <oungsi1000@gmail.com>
2025-01-17 15:53:51 +0900

    수정 버튼으로 배송상태 수정하게끔 변경

--- backend/src/main/java/com/nbe3/cafemanagement/controller/AdminOrderManageController.java
+++ backend/src/main/java/com/nbe3/cafemanagement/controller/AdminOrderManageController.java
@@ -7,6 +7,7 @@ import com.nbe3.cafemanagement.service.AdminOrderManageService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
