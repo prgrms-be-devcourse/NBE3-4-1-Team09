54edcbc2cc28f5eb37b39389676eeb88f0e02ba9
son <handsun000@gmail.com>
2025-01-20 14:39:58 +0900

    주문 내역 조회 메모리 적재 방식으로 인한 날짜 범위로 조회

--- backend/src/main/java/com/nbe3/cafemanagement/dto/OrderDetailDto.java
+++ backend/src/main/java/com/nbe3/cafemanagement/dto/OrderDetailDto.java
@@ -1,12 +1,34 @@
 package com.nbe3.cafemanagement.dto;
 
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.Period;
 
 @Getter
 @Setter
 public class OrderDetailDto {
     @NotEmpty(message = "이메일을 입력하세요")
     private String email;
+    private LocalDate dayFrom = LocalDate.now().minusWeeks(1);
+    private LocalDate dayUntil = LocalDate.now();
+    private int page;
+
+    @AssertTrue(message = "날짜는 최대 1개월간 조회할 수 있습니다")
+    public boolean getIsDateValid() {
+        LocalDate today = LocalDate.now();
+        LocalDate minDate = today.minus(Period.ofMonths(1));
+        return(dayFrom.isAfter(minDate) || dayFrom.isEqual(minDate))
+                &&( dayFrom.isBefore(today) || dayFrom.isEqual(today))
+                && (dayUntil.isAfter(minDate) || dayUntil.isEqual(minDate))
+                &&( dayUntil.isBefore(today) || dayUntil.isEqual(today));
+    }
+
+    @AssertTrue(message = "날짜를 다시 선택해주세요.")
+    public boolean getIsDateCompareValid() {
+        return !dayUntil.isBefore(dayFrom);
+    }
 }
--- backend/src/main/java/com/nbe3/cafemanagement/repository/OrderRepository.java
+++ backend/src/main/java/com/nbe3/cafemanagement/repository/OrderRepository.java
@@ -3,8 +3,11 @@ package com.nbe3.cafemanagement.repository;
 import com.nbe3.cafemanagement.domain.CustomerOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<CustomerOrder, Long> {
     List<CustomerOrder> findByEmailOrderByCreatedAtDesc(String email);
+
+    List<CustomerOrder> findByEmailAndOrderDateBetweenOrderByCreatedAtDesc(String email, LocalDate dayFrom, LocalDate dayUntil);
 }
